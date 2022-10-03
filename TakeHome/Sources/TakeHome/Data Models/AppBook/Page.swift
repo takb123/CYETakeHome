168fdd8fcd0ea1f9595e316479c3a85fc3a45496
Sam DuBois <samdubois@sams-air.home>
2022-10-03 11:59:37 -0400

    Initializer for PAge

--- TakeHome/Sources/TakeHome/Data Models/AppBook/Page.swift
+++ TakeHome/Sources/TakeHome/Data Models/AppBook/Page.swift
@@ -11,6 +11,10 @@ public struct Page: Codable, Identifiable {
     
     public private(set) var id: UUID
     
+    public var title: String
     
-    
+    public init(title: String) {
+        self.id = UUID()
+        self.title = title
+    }
 }
